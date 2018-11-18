baf4eb3d78e54f462bb77eb495fcf3d92aac6e6d
mguevara717 <marissa.guevara717@gmail.com>
2018-11-18 18:53:46 -0500

    Done.

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,6 +5,7 @@ import {
 } from 'react-router-dom';
 import NavBar from '../components/NavBar';
 import MoviesPage from './MoviesPage';
+import MovieList from '../components/MovieList';
 
 class App extends Component {
 
